c3c8e94b1e8ffc98c92aae420bad9bec58aaef1e
Kunal Bhalla <bhalla.kunal@gmail.com>
2020-07-05 16:58:07 -0400

    Add readme, also fix up argument parsing

--- panopticon/__main__.py
+++ panopticon/__main__.py
@@ -4,22 +4,9 @@ import argparse
 import asyncio
 import runpy
 import sys 
+import os
 
 from .tracer import AsyncioTracer
-
-async def sleepy_generator():
-    for x in range(1):
-        await asyncio.sleep(.01)
-        yield x
-
-async def gen_consumer():
-    async for x in sleepy_generator():
-        print(f"Hello,", end=" ")
-
-    async for x in sleepy_generator():
-        print(f"Hello,", end=" ")
-
-    print("World")
 
 def main():
     parser = argparse.ArgumentParser(
@@ -29,22 +16,34 @@ def main():
     parser.add_argument('-o', '--output')
 
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-m', '--module', help='Run module')
     group.add_argument('-c', '--command',
                         help='Run python statements as command')
     group.add_argument('path', nargs='?')
+
+    parser.add_argument('arguments', nargs=argparse.REMAINDER,
+                        help='Arguments to pass to the program')
+
     args = parser.parse_args()
 
-
-    if args.module:
-        with AsyncioTracer() as at:
-            runpy.run_module(args.module)
-    elif args.command:
+    # Adapted from trace.py
+    if args.command:
         with AsyncioTracer() as at:
             eval(args.command)
     elif args.path:
+        sys.argv = [args.path, *args.arguments] 
+        sys.path[0] = os.path.dirname(args.path)
+
+        with open(args.path) as program:
+            code = compile(program.read(), args.path, 'exec', dont_inherit=True)
+        run_globals = {
+            '__file__': args.path,
+            '__name__': '__main__',
+            '__package__': None,
+            '__cached__': None,
+        }
+
         with AsyncioTracer() as at:
-            runpy.run_path(args.path)
+            exec(code, run_globals)
 
     trace = at.get_trace()
     if args.output:
