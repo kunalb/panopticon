fc2fbf55472f4665825325fce4e726211c936481
Kunal Bhalla <bhalla.kunal@gmail.com>
2020-07-31 00:05:14 -0400

    Clean up __init__.py

--- panopticon/__init__.py
+++ panopticon/__init__.py
@@ -1,15 +1,4 @@
 #!/bin/env python3
 
-import sys
-
-from dataclasses import dataclass
-from typing import Any, Dict
 
 from .tracer import AsyncioTracer
-
-def start() -> AsyncioTracer:
-    ...
-
-def stop() -> None:
-    ...
-
