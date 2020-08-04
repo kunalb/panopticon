1e5fee97cbe28017f2b6a29cf20b23eef5ec190e
Kunal Bhalla <bhalla.kunal@gmail.com>
2020-08-04 00:54:54 -0400

    Add a convenience trace() context manager API

--- panopticon/__init__.py
+++ panopticon/__init__.py
@@ -1,5 +1,17 @@
 #!/bin/env python3
 
+from contextlib import contextmanager
+
 import panopticon.version
 
+from panopticon.trace import StreamingTrace
+from panopticon.tracer import AsyncioTracer
+
 __version__ = version.version
+
+
+@contextmanager
+def trace(trace_file: str):
+    with open(trace_file, "w") as out:
+        with AsyncioTracer(trace=StreamingTrace(out)):
+            yield
