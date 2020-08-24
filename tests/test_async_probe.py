516777b93a7c6816c460285a7ca8ec29ff2261d0
Kunal Bhalla <bhalla.kunal@gmail.com>
2020-08-24 00:41:16 -0400

    Completely skip test for python 3.7

--- tests/test_async_probe.py
+++ tests/test_async_probe.py
@@ -10,22 +10,20 @@ from panopticon.trace import StreamingTrace
 from tests.utils import parse_json_trace, record
 
 
-@unittest.skipUnless(
-    sys.version_info >= (3, 8), "Async test requires Python 3.8+"
-)
-class TestAsyncProbe(unittest.IsolatedAsyncioTestCase):
-    async def test_probe_async_function(self):
-        output = io.StringIO()
-        trace = record(StreamingTrace(output))
-
-        @probe(trace)
-        async def test_function():
-            print("Step 1")
-            await asyncio.sleep(0.0001)
-            print("Step 2")
-            await asyncio.sleep(0.0001)
-            print("Step 3")
-            return 4
-
-        await test_function()
-        json_trace = parse_json_trace(output.getvalue())
+if sys.version_info >= (3, 8):
+    class TestAsyncProbe(unittest.IsolatedAsyncioTestCase):
+        async def test_probe_async_function(self):
+            output = io.StringIO()
+            trace = record(StreamingTrace(output))
+    
+            @probe(trace)
+            async def test_function():
+                print("Step 1")
+                await asyncio.sleep(0.0001)
+                print("Step 2")
+                await asyncio.sleep(0.0001)
+                print("Step 3")
+                return 4
+    
+            await test_function()
+            json_trace = parse_json_trace(output.getvalue())
